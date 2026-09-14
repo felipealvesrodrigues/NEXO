e8572f85deb58d85cfde79b1887744aeb3c5f105
Felipe Alves Rodigues <alvesrodriguesfelipe8@gmail.com>
2026-09-14 16:56:41 -0300

    Lógica dos cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,74 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Header() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <header>
+      <h1>Nexo</h1>
+    </header>
+  );
+}
 
-      <div className="ticks"></div>
+interface CardProps {
+  title: string
+}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+function Card({ title }: CardProps) {
+  return (
+    <div>
+      <p>
+        {title}
+      </p>
+    </div>
   )
 }
 
-export default App
+interface ColumnProps{
+  title: string
+  children: React.ReactNode /* Isso aqui permite que componentes sejam colocados dentro de um componente */
+}
+
+function Column({title, children}: ColumnProps ) {
+  return(
+    <div style={{border: '1px solid #fbfcff', padding: '16px', margin: '10px', borderRadius: '8px', minWidth: '250px'}}>
+      <h2>
+        {title}
+      </h2>
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([ /*  */
+    {id: 1, title: 'Aprender props e Children', status: 'todo'},
+    {id: 2, title: 'Entender o useState', status: 'todo'},
+    {id: 3, title: 'Montar a extrutura do NEXO', status: 'in_progress'},
+  ])
+  return (
+    <div>
+      <Header />
+
+      <div style={{display: 'flex', gap: '16px', padding: '16px'}}> {/* Englobei numa div para fazer eles ficarem lado a lado */}
+        <Column title="A fazer">
+          {tasks 
+          .filter((task) => task.status === 'todo')
+          .map((task) => (
+            <Card key={task.id} title={task.title} />
+          ))}
+        </Column> 
+        
+        <Column title="Em progresso">
+        {tasks
+          .filter((task) => task.status === 'in_progress')
+          .map((task)=> (
+            <Card key={task.id} title={task.title} />
+          ))}
+        </Column>
+      </div>
+    </div>
+  );
+}
