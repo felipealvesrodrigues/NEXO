4cfed8ec08f27301402aea75a9ee4f6b71d97892
Felipe Alves Rodigues <alvesrodriguesfelipe8@gmail.com>
2026-09-16 11:50:50 -0300

    elaboração das funções de mover e deletar

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,24 @@ function Header() {
 }
 
 interface CardProps {
+  id: number
   title: string
+  status: string
+  onMove: (id: number, newStatus: string) => void;
+  onDelete: (id: number) => void
 }
 
 function Card({ title }: CardProps) {
   return (
-    <div>
-      <p>
+    <div style={{border: '1px solid #475569', padding: '10px 8px 8px 8px', marginBottom: '10px', borderRadius: '5px'}}>
+      <p style={{margin: '0'}}>
         {title}
       </p>
+
+      <div style={{display: 'flex', gap: '8px', marginTop: '8px' }}>
+
+      </div>
+
     </div>
   )
 }
@@ -31,7 +40,7 @@ interface ColumnProps{
 function Column({title, children}: ColumnProps ) {
   return(
     <div style={{border: '1px solid #fbfcff', padding: '16px', margin: '10px', borderRadius: '8px', minWidth: '250px'}}>
-      <h2>
+      <h2 style={{marginBottom: '10px'}}>
         {title}
       </h2>
       <div>
@@ -41,19 +50,14 @@ function Column({title, children}: ColumnProps ) {
   )
 }
 
-
-
-
-
-
 export default function App() {
   const [tasks, setTasks] = useState([ 
-    {id: 1, title: 'Aprender props e Children', status: 'todo'},
-    {id: 2, title: 'Entender o useState', status: 'todo'},
+    {id: 1, title: 'Aprender props e Children', status: 'to_do'},
+    {id: 2, title: 'Entender o useState', status: 'to_do'},
     {id: 3, title: 'Montar a extrutura do NEXO', status: 'in_progress'},
   ])
-  const [newTaskTitle, setNewTaskTitle] = useState('');
-  const handleAddTask = (e: React.FormEvent) => {
+  const [newTaskTitle, setNewTaskTitle] = useState(''); /* Esse setNewTask vai pegar o que ta escrito lá no form. O setNewTask tbm está diretamente ligado ao campo de escrever o texto*/
+  const handleAddTask = (e: React.FormEvent) => { /* handleAddTask, o parâmetro "e" e o setNewTaskTitle estão conctados. O setNewTaskTitle salva na memória do react o que o "e" capturou da digitação (por meio do e.target.value) e quando Enter ou o botão de enviar é apertado, o handle pega o que tá salvo SetNew e cria uma nova task com ele. */
     e.preventDefault(); /* Previne que a página recarregue ao enviar o formulário */
 
     if (!newTaskTitle.trim()) return; /* Não adiciona se o texto estiver vazio */
@@ -61,23 +65,34 @@ export default function App() {
     const newTask = {
       id: Date.now(), /* Gera um id baseado no tinmestamp atual */
       title: newTaskTitle,
-      status: 'todo' /* Vai fazer com que toda nova task entre na aba de "A fazer" */
+      status: 'to_do' /* Vai fazer com que toda nova task entre na aba de "A fazer" */
     };
 
     setTasks([...tasks, newTask]); /* Adiciona ua nova task mantendo as antigas */
     setNewTaskTitle('')/* Limpa o campo de texto depois de enviar */
   }
 
+  const handleMoveTask = (id: number, newStatus: string) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, status: newStatus} : task
+      )
+    );
+  };
+
+  const handleDeleteTask = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div>
       <Header />
-      
       <form onSubmit={handleAddTask} style={{padding: '16px', display: 'flex', gap: '8px'}}>
         <input
           type="text" 
           placeholder="Digite o título da nova tarefa..."
           value={newTaskTitle}
-          onChange={(e) => setNewTaskTitle(e.target.value)} /* Toda vez que uma letra é digitada no input, o evento e.target.value captura o texto atual e atualiza o estado newTaskTitle*/
+          onChange={(e) => setNewTaskTitle(e.target.value)} /* Toda vez que uma letra é digitada no input, o SetNewTaskTitle captura o evento e.target.value e atualiza o estado newTaskTitle.*/
           style={{padding: '8px', borderRadius: '4px', border: '1px solid #ccc', width: '300px'}}
           />
 
@@ -89,8 +104,8 @@ export default function App() {
       <div style={{display: 'flex', gap: '16px', padding: '16px'}}> {/* Englobei numa div para fazer eles ficarem lado a lado */}
         <Column title="A fazer">
           {tasks 
-          .filter((task) => task.status === 'todo')
-          .map((task) => (
+          .filter((task) => task.status === 'to_do')
+          .map((task) => ( /* O map pega os itens filtrados pelo filter e converte em um componente de react, uma tarefa de cada vez */
             <Card key={task.id} title={task.title} />
           ))}
         </Column> 
@@ -98,7 +113,7 @@ export default function App() {
         <Column title="Em progresso">
         {tasks
           .filter((task) => task.status === 'in_progress')
-          .map((task)=> (
+          .map((task)=> ( /* O map pega os itens filtrados pelo filter e converte em um componente de react, uma tarefa de cada vez */
             <Card key={task.id} title={task.title} />
           ))}
         </Column>
